9aa3687426c542f06ba4cbeaf505ba85ed06dd55
Nicope <n.peugnet@free.fr>
2017-04-09 22:42:42 +0200

    added error handling in login ejs

--- app.js
+++ app.js
@@ -137,15 +137,6 @@ io.on('connection', function(client){
 
 server.listen(3000);
 
-/*connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();*/
-
 function allClients()
 {
 	var clientsList = [];
