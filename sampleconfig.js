8319300e5fd19960cb0ba5f711d9ea57a4a9e182
Nicope <n.peugnet@free.fr>
2017-04-28 16:26:58 +0200

    added README and sampleconfig

---
+++ sampleconfig.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.mysql = {};
+config.web = {};
+
+config.mysql.host = 'localhost';
+config.mysql.user = 'root';
+config.mysql.password = '';
+config.mysql.database = 'polygoneV2';
+config.web.port = 3000;
+
+module.exports = config;
